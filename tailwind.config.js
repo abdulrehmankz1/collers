5f8e0d06de79cc3afd0d1288b8ceff9465bf4ce3
abdulrehmankz1 <khanadaabdulrehman1@gmail.com>
2023-10-23 15:46:37 +0500

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '100px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1500px'
+      },
+    },
+  },
+  plugins: [],
+}
+
